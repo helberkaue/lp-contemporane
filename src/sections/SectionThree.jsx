c36feba22f79247df38afee563e4b3f438de40a6
helberkaue <helber.avelino05@gmail.com>
2025-09-08 12:26:26 -0300

    Atualizando o button 'cruz das almas'

--- src/sections/SectionThree.jsx
+++ src/sections/SectionThree.jsx
@@ -12,19 +12,21 @@ export default function SectionThree() {
                 sizes="100vw"
             />
 
-            <div className="max-w-5xl mx-auto flex flex-col items-center text-center gap-8 px-4 md:px-8 lg:px-16">
+            <div className="max-w-5xl mx-auto flex flex-col items-center text-center gap-10 md:gap-16 px-4 md:px-8 lg:px-16">
                 <p className="text-white sm:text-xl text-3xl md:text-5xl font-bold">
                     O endereço certo para <br />o seu negócio crescer
                 </p>
-
-                <Image
-                    src={"/elements/Dobra 4 - Mapa.png"}
-                    width={800}
-                    height={300}
-                    alt="Mapa"
-                    className="max-w-full h-auto mt-6"
-                    priority
-                />
+                <div className=" flex flex-col items-center text-center">
+                    <Image
+                        src={"/elements/MAPA.png"}
+                        width={900}
+                        height={300}
+                        alt="Mapa"
+                        className="max-w-full h-auto"
+                        priority
+                    />
+                    <div className="rounded-4xl bg-[#66aade] text-xl sm:text-2xl text-white md:text-3xl lg:text-4xl font-semibold py-1 px-12"><p>Cruz das almas</p></div>
+                </div>
 
                 <Image
                     src={"/elements/Dobra 6 Loc.jpeg"}
