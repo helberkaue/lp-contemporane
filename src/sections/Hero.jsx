ea542eb602905c543e6bd265883d5a043748175a
helberkaue <helber.avelino05@gmail.com>
2025-09-10 09:58:06 -0300

    news atualizações

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -13,13 +13,12 @@ export default function Hero() {
         fetchPriority="high"
       />
 
-      <div className="relative mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8
+      <div className="relative mx-auto w-full max-w-8xl px-4 sm:px-6 lg:px-8
                       min-h-[100svh] md:min-h-[100dvh] py-6 sm:py-16 lg:py-32
                       text-white flex flex-col gap-24 justify-between">
 
-        {/* Texto alinhado à direita */}
         <div className="flex-1 flex items-center justify-center md:justify-end">
-          <div className="max-w-4xl flex flex-col gap-4 md:p-32 md:px- sm:gap-6 text-center md:text-right">
+          <div className="max-w-4xl flex flex-col gap-4 md:p-32 md:p sm:gap-6 text-center md:text-right">
             <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight">
               O maior complexo empresarial de Alagoas
             </h1>
@@ -30,7 +29,6 @@ export default function Hero() {
           </div>
         </div>
 
-        {/* Logo centralizado embaixo */}
         <div className="flex justify-center mt-8">
           <Image
             src="/elements/Dobra 1 - Logo.png"
--- src/sections/SectionFour.jsx
+++ src/sections/SectionFour.jsx
@@ -3,25 +3,29 @@ import Image from "next/image"
 export default function SectionFour() {
     return (
         <div className="bg-white min-h-screen flex items-center">
-            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-16 py-16 flex flex-col gap-14 items-center justify-center text-center">
-                
-                <p className="text-2xl sm:text-3xl lg:text-4xl text-blue-500 font-bold">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-16 py-24 flex flex-col gap-14 items-center justify-center text-center">
+                {/* <p className="text-2xl sm:text-3xl lg:text-4xl text-blue-500 font-bold">
                     Quem somos
-                </p>
+                </p> */}
 
                 <div className="flex flex-col items-center justify-center px-4 md:px-8 lg:px-12 gap-12">
-                    
+
                     <div className="flex flex-col md:flex-row items-center gap-10 text-left">
-                        <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-blue-500 leading-relaxed flex-1">
-                            A Contemporânea é uma construtora alagoana com sólida atuação nos segmentos de 
-                            construção civil e incorporação imobiliária. Com mais de 20 anos de experiência e 
-                            um histórico familiar consolidado, a empresa tem como propósito desenvolver 
-                            empreendimentos que aliem qualidade, inovação e segurança. 
-                            Comprometida com os mais altos padrões de engenharia e tecnologia, a Contemporânea 
-                            entrega projetos que atendem às expectativas de um público criterioso, focado na 
-                            valorização patrimonial e na excelência em cada detalhe.
-                        </p>
-                        
+                        <div className="flex flex-col gap-8 items-center ">
+                            <p className="text-2xl sm:text-3xl lg:text-4xl text-blue-500 font-bold">
+                                Quem somos
+                            </p>
+                            <p className="text-left sm:text-lg md:text-xl lg:text-2xl text-[#323232] leading-relaxed flex-1">
+                                A Contemporânea é uma construtora alagoana com sólida atuação nos segmentos de
+                                construção civil e incorporação imobiliária. Com mais de 20 anos de experiência e
+                                um histórico familiar consolidado, a empresa tem como propósito desenvolver
+                                empreendimentos que aliem qualidade, inovação e segurança.
+                                Comprometida com os mais altos padrões de engenharia e tecnologia, a Contemporânea
+                                entrega projetos que atendem às expectativas de um público criterioso, focado na
+                                valorização patrimonial e na excelência em cada detalhe.
+                            </p>
+                        </div>
+
                         <Image
                             src={"/elements/image-quemSomos.jpeg"}
                             width={400}
